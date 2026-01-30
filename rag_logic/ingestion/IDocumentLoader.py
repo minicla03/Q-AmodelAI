d98ffc0be693f42eb5248bc57f3fb4fc51c6f7fb
minicla03 <mininnoclaudio@gmail.com>
2026-01-30 11:49:05 +0100

    chore: init of backend

--- rag_logic/ingestion/IDocumentLoader.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import List
-from langchain_core.documents import Document
-
-class DocumentLoaderStrategy(ABC):
-    @abstractmethod
-    def load(self, file_path: str) -> List[Document]:
-        """Carica e restituisce una lista di Document da un file o sorgente."""
-        pass
